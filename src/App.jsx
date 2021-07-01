1897feb7cf328411280faf8a07a16aaa41124dc4
Pavel Pechkin <pavel.pechkin@sharp-dev.net>
2021-07-02 00:00:47 +0400

    lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
 import Filters from "./components/Filters/Filters.jsx";
 import MovieList from "./components/Movies/MovieList.jsx";
 
@@ -38,31 +39,34 @@ class App extends Component {
 	render() {
 		const { filters, page, total_pages } = this.state;
 		return (
-			<div className="container">
-				<div className="row mt-4">
-					<div className="col-4">
-						<div className="card">
-							<div className="card-body">
-								<h3>Filters</h3>
-								<Filters
-									filters={filters}
-									onChangeFilters={this.onChangeFilters}
-									page={page}
-									total_pages={total_pages}
-									onChangePage={this.onChangePage}
-								/>
+			<React.Fragment>
+				<Header />
+				<div className="container">
+					<div className="row mt-4">
+						<div className="col-4">
+							<div className="card">
+								<div className="card-body">
+									<h3>Filters</h3>
+									<Filters
+										filters={filters}
+										onChangeFilters={this.onChangeFilters}
+										page={page}
+										total_pages={total_pages}
+										onChangePage={this.onChangePage}
+									/>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="col-8">
-						<MovieList
-							filters={filters}
-							page={page}
-							onChangePage={this.onChangePage}
-						/>
+						<div className="col-8">
+							<MovieList
+								filters={filters}
+								page={page}
+								onChangePage={this.onChangePage}
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
